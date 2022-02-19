9ae47c6977d6c072c52c24e37dd46c0f18651afe
MinusKelvin <mark.carlson@minuskelvin.net>
2022-02-20 00:50:41 +1100

    add mvv-lva ordering to qsearch

--- frozenight/src/search/qsearch.rs
+++ frozenight/src/search/qsearch.rs
@@ -1,8 +1,10 @@
-use cozy_chess::Board;
+use cozy_chess::{Piece, Board};
 
 use crate::Eval;
 
 use super::Searcher;
+
+const PIECE_ORDINALS: [i8; Piece::NUM] = [0, 1, 1, 2, 3, 4];
 
 impl Searcher {
     pub fn qsearch(&mut self, board: &Board, mut alpha: Eval, beta: Eval, ply_index: u16) -> Eval {
@@ -19,24 +21,39 @@ impl Searcher {
         }
 
         let capture_squares = board.colors(!board.side_to_move());
+        let mut moves = vec![];
         board.generate_moves(|mut mvs| {
             mvs.to &= capture_squares;
             for mv in mvs {
-                let mut new_board = board.clone();
-                new_board.play_unchecked(mv);
-                let v = -self.qsearch(&new_board, -beta, -alpha, ply_index + 1);
-                if v > best {
-                    best = v;
-                }
-                if v > alpha {
-                    alpha = v;
-                    if v >= beta {
-                        return true;
-                    }
-                }
+                let attacker = PIECE_ORDINALS[mvs.piece as usize];
+                let victim = PIECE_ORDINALS[board.piece_on(mv.to).unwrap() as usize] * 4;
+                moves.push((mv, victim - attacker));
             }
             false
         });
+
+        while !moves.is_empty() {
+            let mut index = 0;
+            for i in 1..moves.len() {
+                if moves[i].1 > moves[index].1 {
+                    index = i;
+                }
+            }
+            let mv = moves.swap_remove(index).0;
+
+            let mut new_board = board.clone();
+            new_board.play_unchecked(mv);
+            let v = -self.qsearch(&new_board, -beta, -alpha, ply_index + 1);
+            if v > best {
+                best = v;
+            }
+            if v > alpha {
+                alpha = v;
+                if v >= beta {
+                    break;
+                }
+            }
+        }
 
         best
     }
