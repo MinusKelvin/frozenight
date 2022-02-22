cc776f34303acd4f2f89515948c531f06defc030
MinusKelvin <mark.carlson@minuskelvin.net>
2022-02-22 18:04:47 +1100

    raise rfp margin even more

--- frozenight/src/search.rs
+++ frozenight/src/search.rs
@@ -141,7 +141,7 @@ impl Searcher {
 
         // reverse futility pruning... but with qsearch
         if depth <= 5 {
-            let margin = 200 * depth as i16;
+            let margin = 250 * depth as i16;
             let eval = self.qsearch(board, beta + margin, beta + margin, ply_index);
             if eval - margin >= beta {
                 return Some(eval);
