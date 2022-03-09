1521c41a488039d37d595d0daeda5da4c3bcb2db
MinusKelvin <mark.carlson@minuskelvin.net>
2022-03-09 12:37:25 +1100

    being reorganizing gen-data

--- gen-data/src/games.rs
+++ gen-data/src/games.rs
@@ -1,134 +1,142 @@
+use std::collections::HashMap;
 use std::fs::OpenOptions;
 use std::io::{stdout, BufWriter, Write};
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::{Arc, Mutex};
 use std::time::Instant;
 
-use cozy_chess::{Board, Color, Piece, Square};
-use cozy_syzygy::Tablebase;
+use cozy_chess::{Board, Color, GameStatus, Move};
+use cozy_syzygy::{Tablebase, Wdl};
+use frozenight::Frozenight;
+use rand::prelude::*;
+use structopt::StructOpt;
 
-use crate::{Options, Sample};
+use crate::CommonOptions;
 
-pub(crate) fn generate_games(options: &Options) {
-    let output = OpenOptions::new()
-        .create_new(true)
-        .write(true)
-        .open("data.bin")
-        .unwrap_or_else(|e| {
-            eprintln!("Could not create data.bin: {}", e);
-            std::process::exit(1)
-        });
-    let output = Arc::new(Mutex::new(BufWriter::new(output)));
-
-    let tb = options
-        .syzygy_path
-        .as_ref()
-        .map(Tablebase::new)
-        .map(Arc::new);
-    if let Some(tb) = tb.as_ref() {
-        println!("Using tablebase adjudication with {} men", tb.max_pieces());
-    }
-    let game_counter = Arc::new(AtomicUsize::new(0));
-    let start = Instant::now();
-    let handles: Vec<_> = (0..options.concurrency)
-        .map(|_| {
-            let tb = tb.clone();
-            let game_counter = game_counter.clone();
-            let output = output.clone();
-            let count = options.count;
-            let depth = options.depth;
-            std::thread::spawn(move || loop {
-                let samples = sample_game(tb.as_deref(), &output, depth);
-                let total = samples + game_counter.fetch_add(samples, Ordering::SeqCst);
-                let completion = total as f64 / count as f64;
-                let time = start.elapsed().as_secs_f64();
-                let eta = time / completion - time;
-                print!(
-                    "\r\x1b[K{:>6.2}% complete. {:.0} samples/sec. Estimated time remaining: {} minutes",
-                    completion * 100.0,
-                    total as f64 / time,
-                    eta as i64 / 60,
-                );
-                stdout().flush().unwrap();
-                if total >= count {
-                    break;
-                }
-            })
-        })
-        .collect();
-    for handle in handles {
-        handle.join().unwrap();
-    }
-    println!();
+#[derive(StructOpt)]
+pub(crate) struct Options {
+    #[structopt(short = "d", long, default_value = "8")]
+    depth: u16,
+    #[structopt(default_value = "10_000_000", parse(try_from_str = crate::parse_filter_underscore))]
+    positions: usize,
 }
 
-fn sample_game(tb: Option<&Tablebase>, output: &Mutex<impl Write>, depth: u16) -> usize {
-    let (game, winner) = super::play_game(depth, tb);
-
-    let mut samples = 0;
-    for sample in game {
-        if sample.board.color_on(sample.mv.to) == Some(!sample.board.side_to_move()) {
-            // skip captures
-            continue;
-        }
-
-        if !sample.board.checkers().is_empty() {
-            // skip in check
-            continue;
-        }
-
-        samples += 1;
-        emit_sample(&mut *output.lock().unwrap(), &sample, winner);
-    }
-
-    samples
-}
-
-fn emit_sample(mut out: impl Write, sample: &Sample, winner: Option<Color>) {
-    write_features(
-        &mut out,
-        &sample.board,
-        sample.board.side_to_move() == Color::Black,
-    );
-    write_features(
-        &mut out,
-        &sample.board,
-        sample.board.side_to_move() == Color::White,
-    );
-    out.write_all(&sample.eval.raw().to_le_bytes()).unwrap();
-    out.write_all(match (winner, sample.board.side_to_move()) {
-        (Some(win), stm) if win == stm => &[2, 0],
-        (Some(win), stm) if win != stm => &[0, 0],
-        (None, _) => &[1, 0],
-        _ => unreachable!(),
-    })
-    .unwrap();
-}
-
-fn write_features(mut out: impl Write, board: &Board, flip: bool) {
-    let color_flip = |c: Color| match flip {
-        false => c,
-        true => !c,
-    };
-    let sq_flip = |sq: Square| match flip {
-        false => sq,
-        true => sq.flip_rank(),
-    };
-    for sq in board.occupied() {
-        let index = feature(
-            color_flip(board.color_on(sq).unwrap()),
-            board.piece_on(sq).unwrap(),
-            sq_flip(sq),
-        );
-        out.write_all(&u16::try_from(index).unwrap().to_le_bytes())
+impl Options {
+    pub(crate) fn run(self, opt: CommonOptions) {
+        let output = OpenOptions::new()
+            .create(true)
+            .append(true)
+            .open("games.dat")
             .unwrap();
-    }
-    for _ in board.occupied().popcnt()..32 {
-        out.write_all(&u16::MAX.to_le_bytes()).unwrap();
-    }
-}
+        let output = Mutex::new(BufWriter::new(output));
 
-// note: duplicate of function in /frozenight/src/nnue.rs
-fn feature(color: Color, piece: Piece, sq: Square) -> usize {
-    sq as usize + Square::NUM * (piece as usize + Piece::NUM * color as usize)
+        let tb = opt.syzygy_path.map(Tablebase::new);
+        if let Some(tb) = tb.as_ref() {
+            println!("Using tablebase adjudication with {} men", tb.max_pieces());
+        }
+        let game_counter = Arc::new(AtomicUsize::new(0));
+        let start = Instant::now();
+
+        crossbeam_utils::thread::scope(|s| {
+            for _ in 0..opt.concurrency {
+                s.spawn(|_| loop {
+                    let (start_pos, mvs, winner) = self.play_game(tb.as_ref());
+
+                    output.lock().map(|mut output| {
+                        write!(output, "{start_pos:#}\t").unwrap();
+                        write!(output, "{}", mvs.first().unwrap()).unwrap();
+                        for mv in &mvs[1..] {
+                            write!(output, " {mv}").unwrap();
+                        }
+                        writeln!(output, "\t{}", match winner {
+                            Some(Color::White) => "1-0",
+                            Some(Color::Black) => "0-1",
+                            None => "1/2-1/2"
+                        }).unwrap();
+                    }).unwrap();
+
+                    let total = mvs.len() + game_counter.fetch_add(mvs.len(), Ordering::SeqCst);
+                    let completion = total as f64 / self.positions as f64;
+                    let time = start.elapsed().as_secs_f64();
+                    let eta = time / completion - time;
+                    print!(
+                        "\r\x1b[K{:>6.2}% complete. {:.0} positions/sec. Estimated time remaining: {} minutes",
+                        completion * 100.0,
+                        total as f64 / time,
+                        eta as i64 / 60,
+                    );
+                    stdout().flush().unwrap();
+                    if total >= self.positions {
+                        break;
+                    }
+                });
+            }
+        })
+        .unwrap();
+        println!();
+    }
+
+    fn generate_starting_position(&self) -> Board {
+        let mut board = Board::default();
+        for _ in 0..8 {
+            let mut moves = vec![];
+            board.generate_moves(|mvs| {
+                moves.extend(mvs);
+                false
+            });
+            if moves.is_empty() {
+                return self.generate_starting_position();
+            }
+            let mv = *moves.choose(&mut thread_rng()).unwrap();
+            board.play_unchecked(mv);
+        }
+        if board.status() != GameStatus::Ongoing {
+            return self.generate_starting_position();
+        }
+        board
+    }
+
+    fn play_game(&self, tb: Option<&Tablebase>) -> (Board, Vec<Move>, Option<Color>) {
+        let start_pos = self.generate_starting_position();
+        let mut repetitions = HashMap::<_, u8>::new();
+        let mut game = vec![];
+
+        let mut engine = Frozenight::new(64);
+        let mut board = start_pos.clone();
+
+        let winner = loop {
+            match board.status() {
+                GameStatus::Won => break Some(!board.side_to_move()),
+                GameStatus::Drawn => break None,
+                GameStatus::Ongoing => {}
+            }
+
+            let entry = repetitions.entry(board.clone()).or_default();
+            *entry += 1;
+            if *entry >= 3 {
+                break None;
+            }
+
+            let mut moves = game.iter().copied();
+            engine.set_position(start_pos.clone(), |_| moves.next());
+
+            let (_, mv) = engine.search_synchronous(None, self.depth, |_, _, _, _, _| {});
+
+            game.push(mv);
+            board.play(mv);
+
+            if let Some(tb) = tb {
+                if board.occupied().popcnt() <= tb.max_pieces() as u32 {
+                    match tb.probe_wdl(&board) {
+                        Some((Wdl::Win, _)) => break Some(board.side_to_move()),
+                        Some((Wdl::Loss, _)) => break Some(!board.side_to_move()),
+                        Some(_) => break None,
+                        None => {}
+                    }
+                }
+            }
+        };
+
+        (start_pos, game, winner)
+    }
 }
--- gen-data/src/main.rs
+++ gen-data/src/main.rs
@@ -1,127 +1,50 @@
-use std::collections::HashMap;
+use std::num::NonZeroUsize;
 use std::path::PathBuf;
 use std::str::FromStr;
 
-use cozy_chess::{Board, Color, GameStatus, Move};
-use cozy_syzygy::{Tablebase, Wdl};
-use frozenight::{Eval, Frozenight};
-use rand::prelude::*;
+use once_cell::sync::Lazy;
 use structopt::StructOpt;
 
 mod games;
 
 #[derive(StructOpt)]
-struct Options {
-    #[structopt(short = "p", long, default_value = "1")]
+struct Args {
+    #[structopt(flatten)]
+    common: CommonOptions,
+
+    #[structopt(subcommand)]
+    subcommand: Subcommand,
+}
+
+#[derive(StructOpt)]
+struct CommonOptions {
+    #[structopt(short = "p", long, default_value = &DEFAULT_CONCURRENCY_STR)]
     concurrency: usize,
     #[structopt(short = "s", long)]
     syzygy_path: Option<PathBuf>,
-    #[structopt(short = "c", long, default_value = "10000000")]
-    count: usize,
-    #[structopt(short = "d", long, default_value = "6")]
-    depth: u16,
-
-    /// One of `wdl`
-    kind: Kind,
 }
 
-enum Kind {
-    Wdl,
-}
-
-impl FromStr for Kind {
-    type Err = std::io::Error;
-
-    fn from_str(s: &str) -> Result<Self, std::io::Error> {
-        Ok(match s {
-            "wdl" => Kind::Wdl,
-            _ => return Err(std::io::ErrorKind::Other.into()),
-        })
-    }
+#[derive(StructOpt)]
+enum Subcommand {
+    /// Generate positions from self-play games
+    Games(games::Options),
 }
 
 fn main() {
-    let options = Options::from_args();
+    let options = Args::from_args();
 
-    match options.kind {
-        Kind::Wdl => games::generate_games(&options),
+    match options.subcommand {
+        Subcommand::Games(opt) => opt.run(options.common),
     }
 }
 
-struct Sample {
-    board: Board,
-    mv: Move,
-    eval: Eval,
-}
+static DEFAULT_CONCURRENCY_STR: Lazy<String> = Lazy::new(|| {
+    std::thread::available_parallelism()
+        .map(NonZeroUsize::get)
+        .unwrap_or(1)
+        .to_string()
+});
 
-fn play_game(depth: u16, tb: Option<&Tablebase>) -> (Vec<Sample>, Option<Color>) {
-    let mut board = Board::default();
-    let mut history = HashMap::<_, u8>::new();
-    let mut game = vec![];
-    for _ in 0..8 {
-        let mut moves = vec![];
-        board.generate_moves(|mvs| {
-            moves.extend(mvs);
-            false
-        });
-        if moves.is_empty() {
-            return (vec![], None);
-        }
-        let entry = history.entry(board.clone()).or_default();
-        *entry += 1;
-        if *entry >= 3 {
-            return (vec![], None);
-        }
-        let mv = *moves.choose(&mut thread_rng()).unwrap();
-        board.play_unchecked(mv);
-        game.push(Sample {
-            mv,
-            eval: Eval::DRAW,
-            board: board.clone(),
-        });
-    }
-    if board.status() != GameStatus::Ongoing {
-        return (vec![], None);
-    }
-
-    let mut engine = Frozenight::new(64);
-
-    let winner = loop {
-        match board.status() {
-            GameStatus::Won => break Some(!board.side_to_move()),
-            GameStatus::Drawn => break None,
-            GameStatus::Ongoing => {}
-        }
-
-        let entry = history.entry(board.clone()).or_default();
-        *entry += 1;
-        if *entry >= 3 {
-            break None;
-        }
-
-        let mut moves = game.iter().map(|s| s.mv);
-        engine.set_position(Board::default(), |_| moves.next());
-
-        let (eval, mv) = engine.search_synchronous(None, depth, |_, _, _, _, _| {});
-
-        game.push(Sample {
-            mv,
-            eval,
-            board: board.clone(),
-        });
-        board.play(mv);
-
-        if let Some(tb) = tb {
-            if board.occupied().popcnt() <= tb.max_pieces() as u32 {
-                match tb.probe_wdl(&board) {
-                    Some((Wdl::Win, _)) => break Some(board.side_to_move()),
-                    Some((Wdl::Loss, _)) => break Some(!board.side_to_move()),
-                    Some(_) => break None,
-                    None => {}
-                }
-            }
-        }
-    };
-
-    (game.into_iter().skip(8).collect(), winner)
+fn parse_filter_underscore<T: FromStr>(s: &str) -> Result<T, T::Err> {
+    s.replace('_', "").parse()
 }
