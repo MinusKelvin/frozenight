8a7fb4b1465ce710ea1adb3cf3255208f6622ba1
MinusKelvin <mark.carlson@minuskelvin.net>
2022-05-07 04:22:00 +0000

    remove engine files when tournament is over
    bench: 4798788

--- scripts/network-tourney.sh
+++ scripts/network-tourney.sh
@@ -8,3 +8,7 @@ do
     ENGINES="$ENGINES -engine name=$EXE cmd=./$EXE"
 done
 cutechess-cli -repeat -games 2 -rounds 50 -tb ~/syzygy/ -resign movecount=5 score=500 -draw movenumber=40 movecount=5 score=15 -concurrency 32 -each proto=uci tc=1+0.01 -openings file=$HOME/4moves_noob.epd format=epd order=random $ENGINES
+for net in $1/*
+do
+    rm "frzn-$(basename $net .ckpt)"
+done
