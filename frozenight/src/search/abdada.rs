31b55ac6882868025d7dee1a3192d028079d8975
MinusKelvin <mark.carlson@minuskelvin.net>
2022-06-08 12:00:29 +1000

    don't do anything about hash collisions in the abdada table
    bench: 4585730

--- frozenight/src/search/abdada.rs
+++ frozenight/src/search/abdada.rs
@@ -1,47 +1,34 @@
-use std::sync::atomic::{AtomicU64, Ordering};
+use std::sync::atomic::{AtomicU8, Ordering};
 
 pub struct AbdadaTable {
-    entries: Box<[AtomicU64]>,
+    entries: Box<[AtomicU8]>,
 }
 
 impl AbdadaTable {
     pub fn new() -> Self {
         AbdadaTable {
-            entries: (0..65536).map(|_| AtomicU64::new(0)).collect(),
+            entries: (0..65536).map(|_| AtomicU8::new(0)).collect(),
         }
     }
 
     pub fn is_searching(&self, hash: u64) -> bool {
         let entry = &self.entries[(hash % self.entries.len() as u64) as usize];
-        let v = entry.load(Ordering::Acquire);
-        v & !0xFF == hash & !0xFF && v & 0xFF > 0
+        entry.load(Ordering::Acquire) > 0
     }
 
     pub fn enter(&self, hash: u64) -> Option<AbdadaGuard> {
         let entry = &self.entries[(hash % self.entries.len() as u64) as usize];
-        let hash_upper = hash & !0xFF;
-        entry
-            .fetch_update(Ordering::AcqRel, Ordering::Acquire, |current| {
-                let count = current & 0xFF;
-                if current & !0xFF == hash_upper && count < 0xFF {
-                    Some(current + 1)
-                } else if count == 0 {
-                    Some(hash_upper + 1)
-                } else {
-                    None
-                }
-            })
-            .ok()
-            .map(|_| AbdadaGuard { entry })
+        entry.fetch_add(1, Ordering::Acquire);
+        Some(AbdadaGuard { entry })
     }
 }
 
 pub struct AbdadaGuard<'a> {
-    entry: &'a AtomicU64,
+    entry: &'a AtomicU8,
 }
 
 impl Drop for AbdadaGuard<'_> {
     fn drop(&mut self) {
-        self.entry.fetch_sub(1, Ordering::AcqRel);
+        self.entry.fetch_sub(1, Ordering::Release);
     }
 }
