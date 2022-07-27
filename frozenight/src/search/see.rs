c1128bb2143b1d21cb142685e1c65b3fabe9c045
MinusKelvin <mark.carlson@minuskelvin.net>
2022-07-27 23:15:37 +1000

    implement straightforward iterative SEE algorithm
    bench: 3789298

--- frozenight/src/search/see.rs
+++ frozenight/src/search/see.rs
@@ -1,89 +1,110 @@
 use cozy_chess::{
     get_bishop_moves, get_king_moves, get_knight_moves, get_pawn_attacks, get_rook_moves, BitBoard,
-    Board, Color, Move, Piece, Square,
+    Board, Move, Piece, Square,
 };
 
-const VALUES: [i32; Piece::NUM] = [100, 300, 325, 500, 900, 0];
+const VALUES: [i32; Piece::NUM] = [100, 300, 325, 500, 900, 9999999];
 
 pub fn static_exchange_eval(board: &Board, capture: Move) -> i32 {
     let occupied = board.occupied() & !capture.from.bitboard();
     VALUES[board.piece_on(capture.to).unwrap() as usize]
         - see_impl(
             board,
-            !board.side_to_move(),
             capture.to,
             board.piece_on(capture.from).unwrap(),
             occupied,
-            get_bishop_moves(capture.to, occupied),
-            get_rook_moves(capture.to, occupied),
         )
 }
 
-fn see_impl(
-    board: &Board,
-    stm: Color,
-    sq: Square,
-    to_capture: Piece,
-    occupied: BitBoard,
-    bishop_attacks: BitBoard,
-    rook_attacks: BitBoard,
-) -> i32 {
-    let movable = board.colors(stm) & occupied;
-    let gain = VALUES[to_capture as usize];
+fn see_impl(board: &Board, sq: Square, mut piece: Piece, mut occupied: BitBoard) -> i32 {
+    let mut stm = board.side_to_move();
+    let mut bishop_attacks = get_bishop_moves(sq, occupied);
+    let mut rook_attacks = get_rook_moves(sq, occupied);
 
-    let eval = |from: Square, piece: Piece| {
-        if to_capture == Piece::King {
-            return 999999;
+    let mut gains = [0; 32];
+    let mut index = 0;
+    for i in 0..32 {
+        index = i;
+
+        stm = !stm;
+        let movable = board.colors(stm) & occupied;
+
+        if let Some(from) =
+            (get_pawn_attacks(sq, !stm) & board.pieces(Piece::Pawn) & movable).next_square()
+        {
+            // Pawn
+            gains[i] = VALUES[piece as usize];
+            if piece == Piece::King {
+                break;
+            }
+            piece = Piece::Pawn;
+            occupied &= !from.bitboard();
+            bishop_attacks = get_bishop_moves(sq, occupied);
+        } else if let Some(from) =
+            (get_knight_moves(sq) & board.pieces(Piece::Knight) & movable).next_square()
+        {
+            // Knight
+            gains[i] = VALUES[piece as usize];
+            if piece == Piece::King {
+                break;
+            }
+            piece = Piece::Knight;
+            occupied &= !from.bitboard();
+        } else if let Some(from) =
+            (bishop_attacks & board.pieces(Piece::Bishop) & movable).next_square()
+        {
+            // Bishop
+            gains[i] = VALUES[piece as usize];
+            if piece == Piece::King {
+                break;
+            }
+            piece = Piece::Bishop;
+            occupied &= !from.bitboard();
+            bishop_attacks = get_bishop_moves(sq, occupied);
+        } else if let Some(from) =
+            (rook_attacks & board.pieces(Piece::Rook) & movable).next_square()
+        {
+            // Rook
+            gains[i] = VALUES[piece as usize];
+            if piece == Piece::King {
+                break;
+            }
+            piece = Piece::Rook;
+            occupied &= !from.bitboard();
+            rook_attacks = get_rook_moves(sq, occupied);
+        } else if let Some(from) =
+            ((rook_attacks | bishop_attacks) & board.pieces(Piece::Queen) & movable).next_square()
+        {
+            // Queen
+            gains[i] = VALUES[piece as usize];
+            if piece == Piece::King {
+                break;
+            }
+            piece = Piece::Queen;
+            occupied &= !from.bitboard();
+            if bishop_attacks.has(from) {
+                bishop_attacks = get_bishop_moves(sq, occupied);
+            } else {
+                rook_attacks = get_rook_moves(sq, occupied);
+            }
+        } else if get_king_moves(sq).has(board.king(stm)) {
+            // King
+            gains[i] = VALUES[piece as usize];
+            if piece == Piece::King {
+                break;
+            }
+            piece = Piece::King;
+            occupied &= !board.king(stm).bitboard();
+        } else {
+            // No more captures possible
+            break;
         }
-        let occupied = occupied & !from.bitboard();
-        let bishop_attacks = match bishop_attacks.has(from) {
-            true => get_bishop_moves(sq, occupied),
-            false => bishop_attacks,
-        };
-        let rook_attacks = match rook_attacks.has(from) {
-            true => get_rook_moves(sq, occupied),
-            false => rook_attacks,
-        };
-        0i32.max(
-            gain - see_impl(
-                board,
-                !stm,
-                sq,
-                piece,
-                occupied,
-                bishop_attacks,
-                rook_attacks,
-            ),
-        )
-    };
-
-    if let Some(sq) =
-        (get_pawn_attacks(sq, !stm) & board.pieces(Piece::Pawn) & movable).next_square()
-    {
-        return eval(sq, Piece::Pawn);
     }
 
-    let knight_attacks = get_knight_moves(sq);
-    if let Some(sq) = (knight_attacks & board.pieces(Piece::Knight) & movable).next_square() {
-        return eval(sq, Piece::Knight);
+    let mut value = 0;
+    for i in (0..=index).rev() {
+        value = 0.max(gains[i] - value);
     }
 
-    if let Some(sq) = (bishop_attacks & board.pieces(Piece::Bishop) & movable).next_square() {
-        return eval(sq, Piece::Bishop);
-    }
-
-    if let Some(sq) = (rook_attacks & board.pieces(Piece::Rook) & movable).next_square() {
-        return eval(sq, Piece::Rook);
-    }
-
-    let queen_attacks = bishop_attacks | rook_attacks;
-    if let Some(sq) = (queen_attacks & board.pieces(Piece::Queen) & movable).next_square() {
-        return eval(sq, Piece::Queen);
-    }
-
-    if get_king_moves(sq).has(board.king(stm)) {
-        return eval(sq, Piece::King);
-    }
-
-    0
+    value
 }
