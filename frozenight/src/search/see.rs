4179a8df4ff10a6a88172ed00a88a0dd0af62bb5
MinusKelvin <mark.carlson@minuskelvin.net>
2022-07-27 17:34:11 +1000

    make SEE a bit faster by not redoing movegen
    bench: 3789298

--- frozenight/src/search/see.rs
+++ frozenight/src/search/see.rs
@@ -6,25 +6,55 @@ use cozy_chess::{
 const VALUES: [i32; Piece::NUM] = [100, 300, 325, 500, 900, 0];
 
 pub fn static_exchange_eval(board: &Board, capture: Move) -> i32 {
+    let occupied = board.occupied() & !capture.from.bitboard();
     VALUES[board.piece_on(capture.to).unwrap() as usize]
         - see_impl(
             board,
             !board.side_to_move(),
             capture.to,
             board.piece_on(capture.from).unwrap(),
-            capture.from.bitboard(),
+            occupied,
+            get_bishop_moves(capture.to, occupied),
+            get_rook_moves(capture.to, occupied),
         )
 }
 
-fn see_impl(board: &Board, stm: Color, sq: Square, to_capture: Piece, moved: BitBoard) -> i32 {
-    let movable = board.colors(stm) & !moved;
+fn see_impl(
+    board: &Board,
+    stm: Color,
+    sq: Square,
+    to_capture: Piece,
+    occupied: BitBoard,
+    bishop_attacks: BitBoard,
+    rook_attacks: BitBoard,
+) -> i32 {
+    let movable = board.colors(stm) & occupied;
     let gain = VALUES[to_capture as usize];
 
     let eval = |from: Square, piece: Piece| {
         if to_capture == Piece::King {
             return 999999;
         }
-        0i32.max(gain - see_impl(board, !stm, sq, piece, moved | from.bitboard()))
+        let occupied = occupied & !from.bitboard();
+        let bishop_attacks = match bishop_attacks.has(from) {
+            true => get_bishop_moves(sq, occupied),
+            false => bishop_attacks,
+        };
+        let rook_attacks = match rook_attacks.has(from) {
+            true => get_rook_moves(sq, occupied),
+            false => rook_attacks,
+        };
+        0i32.max(
+            gain - see_impl(
+                board,
+                !stm,
+                sq,
+                piece,
+                occupied,
+                bishop_attacks,
+                rook_attacks,
+            ),
+        )
     };
 
     if let Some(sq) =
@@ -38,12 +68,10 @@ fn see_impl(board: &Board, stm: Color, sq: Square, to_capture: Piece, moved: Bit
         return eval(sq, Piece::Knight);
     }
 
-    let bishop_attacks = get_bishop_moves(sq, board.occupied() & !moved);
     if let Some(sq) = (bishop_attacks & board.pieces(Piece::Bishop) & movable).next_square() {
         return eval(sq, Piece::Bishop);
     }
 
-    let rook_attacks = get_rook_moves(sq, board.occupied() & !moved);
     if let Some(sq) = (rook_attacks & board.pieces(Piece::Rook) & movable).next_square() {
         return eval(sq, Piece::Rook);
     }
