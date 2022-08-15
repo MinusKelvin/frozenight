15cfa854a24664cbc034c6dc1222cbb05084ba01
MinusKelvin <mark.carlson@minuskelvin.net>
2022-08-15 20:55:36 +1000

    use formula for nmp reduction
    bench: 4122062

--- frozenight/src/search/null.rs
+++ frozenight/src/search/null.rs
@@ -58,10 +58,7 @@ impl Searcher<'_> {
                 | position.board.pieces(Piece::Queen);
             if !(sliders & position.board.colors(position.board.side_to_move())).is_empty() {
                 if let Some(nm) = position.null_move() {
-                    let reduction = match () {
-                        _ if depth > 6 => 4,
-                        _ => 3,
-                    };
+                    let reduction = depth / 2;
                     let v = -self.visit_null(&nm, -window, depth - reduction - 1)?;
                     if window.fail_high(v) {
                         return Some(v);
