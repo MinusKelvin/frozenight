b5e5b625bc9baea35ed65b6a6d5fcb97dc79602c
MinusKelvin <mark.carlson@minuskelvin.net>
2022-09-08 20:00:31 +1000

    new and improved scripts
    bench: 4007851

--- scripts/network-tourney.sh
+++ scripts/network-tourney.sh
@@ -1,14 +1,30 @@
 #!/bin/sh
-for net in $1/*
-do
-    EXE="frzn-$(basename $net .ckpt)"
-    echo Building $EXE
-    scripts/trainer.py dump $net
-    make EXE=$EXE
-    ENGINES="$ENGINES -engine name=$EXE cmd=./$EXE"
+
+CUTECHESS_ARGS="-repeat -recover -games 5000 -tournament knockout -concurrency 16"
+CUTECHESS_ARGS="$CUTECHESS_ARGS -openings file=4moves_noob.epd format=epd order=random"
+CUTECHESS_ARGS="$CUTECHESS_ARGS -draw movenumber=40 movecount=5 score=10"
+CUTECHESS_ARGS="$CUTECHESS_ARGS -resign movecount=4 score=500"
+CUTECHESS_ARGS="$CUTECHESS_ARGS -each nodes=10000 proto=uci"
+
+mkdir -p builds
+for net in nn/*.json; do
+    scripts/json-to-frozenight.py "$net" ../frozenight/frozenight/model.rs
+    cargo build --release
+    NETVER=`$(basename "$net" .json)`
+    cp ../frozenight/target/release/frozenight-uci builds/$NETVER
+    CUTECHESS_ARGS="$CUTECHESS_ARGS -engine name=$NETVER cmd=builds/$NETVER"
 done
-cutechess-cli -repeat -games 2 -rounds 50 -tb ~/syzygy/ -resign movecount=5 score=500 -draw movenumber=40 movecount=5 score=15 -concurrency 32 -each proto=uci tc=1+0.01 -openings file=$HOME/4moves_noob.epd format=epd order=random $ENGINES
-for net in $1/*
-do
-    rm "frzn-$(basename $net .ckpt)"
-done
+
+FIND_WINNER=<<-AWK
+{
+    if (match($0, /^\t+/) && RLENGTH > best) {
+        best = RLENGTH;
+        engine = $1;
+    }
+}
+END {
+    print "WINNER:", engine;
+}
+AWK
+
+cutechess-cli $CUTECHESS_ARGS | tee >(awk "$FIND_WINNER")
