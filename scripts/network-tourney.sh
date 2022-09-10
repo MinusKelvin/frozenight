fa332a9831c17f90ed712a717f4decd5935f673f
MinusKelvin <mark.carlson@minuskelvin.net>
2022-09-10 04:17:32 +0000

    improve network-tourney script
    bench: 4007851

--- scripts/network-tourney.sh
+++ scripts/network-tourney.sh
@@ -1,30 +1,39 @@
-#!/bin/sh
+#!/bin/bash
+set -e
 
 CUTECHESS_ARGS="-repeat -recover -games 5000 -tournament knockout -concurrency 16"
-CUTECHESS_ARGS="$CUTECHESS_ARGS -openings file=4moves_noob.epd format=epd order=random"
+CUTECHESS_ARGS="$CUTECHESS_ARGS -openings file=$HOME/4moves_noob.epd format=epd order=random"
 CUTECHESS_ARGS="$CUTECHESS_ARGS -draw movenumber=40 movecount=5 score=10"
 CUTECHESS_ARGS="$CUTECHESS_ARGS -resign movecount=4 score=500"
-CUTECHESS_ARGS="$CUTECHESS_ARGS -each nodes=10000 proto=uci"
+CUTECHESS_ARGS="$CUTECHESS_ARGS -each nodes=32000 proto=uci tc=inf"
 
-mkdir -p builds
-for net in nn/*.json; do
-    scripts/json-to-frozenight.py "$net" ../frozenight/frozenight/model.rs
-    cargo build --release
-    NETVER=`$(basename "$net" .json)`
-    cp ../frozenight/target/release/frozenight-uci builds/$NETVER
-    CUTECHESS_ARGS="$CUTECHESS_ARGS -engine name=$NETVER cmd=builds/$NETVER"
+[ -e .tmp-builds ] && rm -r .tmp-builds
+[ -e .tmp-networks ] && rm -r .tmp-networks
+mkdir -p .tmp-builds .tmp-networks
+tar --zstd -xf "$1" -C .tmp-networks
+for net in .tmp-networks/*.json; do
+    scripts/json-to-frozenight.py "$net" frozenight/model.rs
+    cargo build --release --bin frozenight-uci
+    NETVER=$(basename "$net" .json)
+    cp target/release/frozenight-uci .tmp-builds/$NETVER
+    CUTECHESS_ARGS="$CUTECHESS_ARGS -engine name=$NETVER cmd=.tmp-builds/$NETVER"
 done
 
-FIND_WINNER=<<-AWK
+FIND_WINNER=$(cat <<AWK
 {
-    if (match($0, /^\t+/) && RLENGTH > best) {
+    if (match(\$0, /^\\t+/) && RLENGTH > best) {
         best = RLENGTH;
-        engine = $1;
+        engine = \$1;
     }
 }
 END {
-    print "WINNER:", engine;
+    print engine;
 }
 AWK
+)
 
-cutechess-cli $CUTECHESS_ARGS | tee >(awk "$FIND_WINNER")
+WINNER=$(cutechess-cli $CUTECHESS_ARGS | tee -a /dev/stderr | awk "$FIND_WINNER")
+cp ".tmp-networks/$WINNER.json" `dirname "$1"`/`basename "$1" .tar.zst`.json
+echo winner: $WINNER
+
+rm -r .tmp-builds .tmp-networks
