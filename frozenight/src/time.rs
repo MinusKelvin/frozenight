8bc40aab02d719b7446b1bf86b09a34c7d328f8b
MinusKelvin <mark.carlson@minuskelvin.net>
2022-09-19 23:53:25 +1000

    experimental dynamic time control implementation
    bench: 3550049

--- frozenight/src/time.rs
+++ frozenight/src/time.rs
@@ -1,9 +1,10 @@
 use std::ops::ControlFlow;
 use std::time::{Duration, Instant};
 
-use cozy_chess::Board;
+use cozy_chess::{Board, Move};
 
-use crate::SearchInfo;
+use crate::search::INVALID_MOVE;
+use crate::{Eval, SearchInfo};
 
 #[derive(Copy, Clone, Debug)]
 pub struct TimeConstraint {
@@ -29,31 +30,66 @@ impl TimeConstraint {
 }
 
 pub(crate) struct TimeManager {
+    board: Board,
     soft_deadline: Option<Instant>,
     hard_deadline: Option<Instant>,
+    soft_deadline_change: Duration,
     one_reply: bool,
+    prev_best_move: Move,
+    prev_best_eval: Eval,
 }
 
 impl TimeManager {
     pub fn new(board: &Board, time: TimeConstraint) -> Self {
         let now = Instant::now();
+        let clock = match time.clock {
+            Some(v) => v,
+            None => {
+                return TimeManager {
+                    board: board.clone(),
+                    one_reply: false,
+                    hard_deadline: None,
+                    soft_deadline: None,
+                    soft_deadline_change: Duration::ZERO,
+                    prev_best_eval: Eval::DRAW,
+                    prev_best_move: INVALID_MOVE,
+                }
+            }
+        };
+
+        if time.use_all_time {
+            return TimeManager {
+                board: board.clone(),
+                one_reply: false,
+                hard_deadline: Some(now + clock.saturating_sub(time.overhead)),
+                soft_deadline: None,
+                soft_deadline_change: Duration::ZERO,
+                prev_best_eval: Eval::DRAW,
+                prev_best_move: INVALID_MOVE,
+            };
+        }
+
+        let mtg = time.moves_to_go.unwrap_or(40).clamp(1, 40);
+
+        let hard_deadline = match mtg {
+            0..=10 => clock * 20 / (20 - mtg),
+            _ => clock / 2,
+        }
+        .saturating_sub(time.overhead);
+
+        let soft_deadline_change = clock / 100;
+        let initial_soft_deadline = (clock.saturating_sub(time.increment) / (mtg + 1))
+            .saturating_add(time.increment)
+            .saturating_sub(time.overhead);
+
         TimeManager {
-            one_reply: !time.use_all_time && time.clock.is_some() && one_reply(board),
-            hard_deadline: time
-                .clock
-                .map(|clock| now + (clock / 2).saturating_sub(time.overhead)),
-            soft_deadline: time
-                .clock
-                .map(|clock| {
-                    if time.use_all_time {
-                        return clock;
-                    }
-
-                    let mtg = time.moves_to_go.unwrap_or(45) + 5;
-
-                    clock.saturating_sub(time.increment) / mtg + time.increment / 2
-                })
-                .map(|amt| now + (amt.saturating_sub(time.overhead))),
+            board: board.clone(),
+            one_reply: one_reply(board),
+            hard_deadline: Some(now + hard_deadline),
+            soft_deadline: Some(now + initial_soft_deadline),
+            soft_deadline_change,
+            prev_best_eval: Eval::DRAW,
+            prev_best_move: INVALID_MOVE,
         }
     }
 
@@ -61,12 +97,31 @@ impl TimeManager {
         self.hard_deadline
     }
 
-    pub fn update(&mut self, _info: &SearchInfo) -> ControlFlow<()> {
+    pub fn update(&mut self, info: &SearchInfo) -> ControlFlow<()> {
         match self.soft_deadline {
             _ if self.one_reply => ControlFlow::Break(()),
             None => ControlFlow::Continue(()),
-            Some(deadline) => {
-                if Instant::now() < deadline {
+            Some(ref mut deadline) => {
+                let now = Instant::now();
+
+                let prev_noisy =
+                    self.prev_best_move != INVALID_MOVE && noisy(&self.board, self.prev_best_move);
+                let new_noisy = noisy(&self.board, info.best_move);
+
+                // reduce time if best move is noisy
+                if new_noisy {
+                    *deadline -= self.soft_deadline_change;
+                }
+
+                // increase time if best move changes from noisy to quiet
+                if prev_noisy && !new_noisy {
+                    *deadline += self.soft_deadline_change * 4;
+                }
+
+                self.prev_best_eval = info.eval;
+                self.prev_best_move = info.best_move;
+
+                if now < *deadline {
                     ControlFlow::Continue(())
                 } else {
                     ControlFlow::Break(())
@@ -83,4 +138,13 @@ fn one_reply(board: &Board) -> bool {
         moves > 1
     });
     moves == 1
+}
+
+fn noisy(board: &Board, mv: Move) -> bool {
+    if board.color_on(mv.to) == Some(!board.side_to_move()) {
+        return true;
+    }
+    let mut b = board.clone();
+    b.play_unchecked(mv);
+    !b.checkers().is_empty()
 }
