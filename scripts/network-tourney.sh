35d18ccfdfa7ed989b05f3e03a568f005ea9c8cd
MinusKelvin <mark.carlson@minuskelvin.net>
2022-10-22 17:41:19 +1100

    update network tournament script
    bench: 5361025

--- scripts/network-tourney.sh
+++ scripts/network-tourney.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-set -e
 
-CUTECHESS_ARGS="-repeat -recover -games 1000 -tournament knockout -concurrency 16"
+CUTECHESS_ARGS="-repeat -recover -games 2 -concurrency 16"
 CUTECHESS_ARGS="$CUTECHESS_ARGS -openings file=$HOME/4moves_noob.epd format=epd order=random"
 CUTECHESS_ARGS="$CUTECHESS_ARGS -draw movenumber=40 movecount=5 score=10"
 CUTECHESS_ARGS="$CUTECHESS_ARGS -resign movecount=4 score=500"
@@ -10,29 +9,34 @@ CUTECHESS_ARGS="$CUTECHESS_ARGS -each nodes=32000 proto=uci tc=inf"
 [ -e .tmp-builds ] && rm -r .tmp-builds
 [ -e .tmp-networks ] && rm -r .tmp-networks
 mkdir -p .tmp-builds .tmp-networks
-tar --zstd -xf "$1" -C .tmp-networks
-for net in .tmp-networks/*.json; do
-    EVALFILE="$net" cargo build --release --bin frozenight-uci
-    NETVER=$(basename "$net" .json)
-    cp target/release/frozenight-uci .tmp-builds/$NETVER
-    CUTECHESS_ARGS="$CUTECHESS_ARGS -engine name=$NETVER cmd=.tmp-builds/$NETVER"
+
+CUTOFF="$1"
+shift 1
+
+let COUNT=0
+for net in "$@"; do
+    tar --zstd -xf "$net" -C .tmp-networks
+    for n in .tmp-networks/*.json; do
+        zstd -19 $n
+    done
+    let i=$CUTOFF
+    while true; do
+        if [ ! -f .tmp-networks/*-$i.json.zst ]; then
+            break
+        fi
+        for ef in .tmp-networks/*-$i.json.zst; do
+            EVALFILE="$ef" cargo build --release --bin frozenight-uci
+            NAME=`basename $net .tar.zst`_`basename $ef .json.zst`
+            cp target/release/frozenight-uci .tmp-builds/$NAME
+            CUTECHESS_ARGS="$CUTECHESS_ARGS -engine name=$NAME cmd=.tmp-builds/$NAME"
+            let COUNT++
+        done
+        let i++
+    done
+    rm .tmp-networks/*
 done
 
-FIND_WINNER=$(cat <<AWK
-{
-    if (match(\$0, /^\\t+/) && RLENGTH > best) {
-        best = RLENGTH;
-        engine = \$1;
-    }
-}
-END {
-    print engine;
-}
-AWK
-)
+let BY=$COUNT-1
+let "ROUNDS = (2000 + $BY-1) / $BY"
 
-WINNER=$(cutechess-cli $CUTECHESS_ARGS | tee -a /dev/stderr | awk "$FIND_WINNER")
-cp ".tmp-networks/$WINNER.json" `dirname "$1"`/`basename "$1" .tar.zst`.json
-echo winner: $WINNER
-
-rm -r .tmp-builds .tmp-networks
+nice cutechess-cli $CUTECHESS_ARGS -rounds $ROUNDS
