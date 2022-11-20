48ec89f86af678c4a3b8ad707961ec7219f00f74
MinusKelvin <mark.carlson@minuskelvin.net>
2022-11-20 23:52:32 +1100

    use temporary directory for nets and builds
    bench: 5925835

--- scripts/network-tourney.sh
+++ scripts/network-tourney.sh
@@ -6,9 +6,8 @@ CUTECHESS_ARGS="$CUTECHESS_ARGS -draw movenumber=40 movecount=5 score=10"
 CUTECHESS_ARGS="$CUTECHESS_ARGS -resign movecount=4 score=500"
 CUTECHESS_ARGS="$CUTECHESS_ARGS -each nodes=32000 proto=uci tc=inf"
 
-[ -e .tmp-builds ] && rm -r .tmp-builds
-[ -e .tmp-networks ] && rm -r .tmp-networks
-mkdir -p .tmp-builds .tmp-networks
+BUILDS=`mktemp -d`
+NETS=`mktemp -d`
 
 CUTOFF="$1"
 shift 1
@@ -16,23 +15,23 @@ shift 1
 let COUNT=0
 for net in "$@"; do
     NET_NAME=`basename $net .tar.zst`
-    tar --zstd -xf "$net" -C .tmp-networks
-    for n in .tmp-networks/*.json; do
+    tar xaf "$net" -C "$NETS"
+    for n in "$NETS"/*.json; do
         NAME=$NET_NAME-`basename $n .json`
-        zstd -19 --rm $n -o .tmp-networks/$NAME.json.zst
+        zstd -19 --rm "$n" -o "$NETS/$NAME.json.zst"
     done
 done
 
 let i=$CUTOFF
 while true; do
-    if [ ! -f .tmp-networks/*-$i.json.zst ]; then
+    if [ ! -f "$NETS"/*-$i.json.zst ]; then
         break
     fi
-    for ef in .tmp-networks/*-$i.json.zst; do
+    for ef in "$NETS"/*-$i.json.zst; do
         EVALFILE="$ef" cargo build --release --bin frozenight-uci
-        NAME=`basename $ef .json.zst`
-        cp target/release/frozenight-uci .tmp-builds/$NAME
-        CUTECHESS_ARGS="$CUTECHESS_ARGS -engine name=$NAME cmd=.tmp-builds/$NAME"
+        NAME=`basename "$ef" .json.zst`
+        cp target/release/frozenight-uci "$BUILDS/$NAME"
+        CUTECHESS_ARGS="$CUTECHESS_ARGS -engine name=$NAME cmd=$BUILDS/$NAME"
         let COUNT++
     done
     let i++
@@ -42,4 +41,7 @@ let BY=$COUNT-1
 let "ROUNDS = (2000 + $BY-1) / $BY"
 
 WINNER=$(nice cutechess-cli $CUTECHESS_ARGS -rounds $ROUNDS | tee -a /dev/stderr | awk '/^\s*1\s/ { print($2) }' )
-cp .tmp-networks/$WINNER.json.zst .
+cp "$NETS"/$WINNER.json.zst .
+echo "$WINNER"
+
+rm -rf "$NETS" "$BUILDS"
