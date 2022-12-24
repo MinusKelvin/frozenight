85e6124134e37b8cd57d38a0e7040e3a1724e20c
MinusKelvin <mark.carlson@minuskelvin.net>
2022-12-24 14:49:42 +1100

    adjust SEE values so MVV-LVA can't reorder
    bench: 5496134

--- frozenight/src/search/see.rs
+++ frozenight/src/search/see.rs
@@ -3,7 +3,7 @@ use cozy_chess::{
     Board, Move, Piece, Square,
 };
 
-const VALUES: [i32; Piece::NUM] = [100, 300, 325, 500, 900, 9999999];
+const VALUES: [i32; Piece::NUM] = [100, 285, 335, 500, 900, 9999999];
 
 pub fn static_exchange_eval(board: &Board, capture: Move) -> i32 {
     let occupied = board.occupied() & !capture.from.bitboard();
