a477c280d85f55b8324b2e334c299ce0275f4d4c
MinusKelvin <mark.carlson@minuskelvin.net>
2023-03-14 13:28:16 +1100

    use tt in qsearch
    bench: 7457347

--- frozenight/src/search/qsearch.rs
+++ frozenight/src/search/qsearch.rs
@@ -3,6 +3,7 @@ use std::sync::atomic::Ordering;
 use cozy_chess::Move;
 
 use crate::position::Position;
+use crate::tt::{NodeKind, TableEntry};
 use crate::Eval;
 
 use super::negamax::SearchType;
@@ -18,6 +19,18 @@ impl Searcher<'_> {
         mut window: Window,
     ) -> Option<(Eval, Option<Move>)> {
         self.stats.nodes.fetch_add(1, Ordering::Relaxed);
+
+        let tt = self.tt.get(pos);
+        if let Some(tt) = tt {
+            let bound_allows_cutoff = match tt.kind {
+                NodeKind::Exact => true,
+                NodeKind::LowerBound => window.fail_high(tt.eval),
+                NodeKind::UpperBound => window.fail_low(tt.eval),
+            };
+            if bound_allows_cutoff {
+                return Some((tt.eval, Some(tt.mv)));
+            }
+        }
 
         let mut best = pos.static_eval();
         let mut best_mv = None;
@@ -36,14 +49,16 @@ impl Searcher<'_> {
                 if see >= 0 {
                     moves.push((
                         mv,
-                        pos.board.piece_on(mv.to).unwrap() as i16 * 8 - mvs.piece as i16,
+                        (Some(mv) == tt.map(|tt| tt.mv)) as i16 * 1000
+                            + pos.board.piece_on(mv.to).unwrap() as i16 * 8
+                            - mvs.piece as i16,
                     ));
                 }
             }
             false
         });
 
-        while let Some((i, &(mv, score))) = moves
+        while let Some((i, &(mv, _))) = moves
             .iter()
             .enumerate()
             .max_by_key(|&(_, &(_, score))| score)
@@ -64,6 +79,22 @@ impl Searcher<'_> {
             }
 
             raised_alpha |= window.raise_lb(v);
+        }
+
+        if let Some(best_mv) = best_mv {
+            self.tt.store(
+                pos,
+                TableEntry {
+                    mv: best_mv,
+                    eval: best,
+                    depth: 0,
+                    kind: match () {
+                        _ if window.fail_high(best) => NodeKind::LowerBound,
+                        _ if raised_alpha => NodeKind::Exact,
+                        _ => NodeKind::UpperBound,
+                    },
+                },
+            );
         }
 
         Some((best, best_mv))
