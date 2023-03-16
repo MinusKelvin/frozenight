2764c641e881e9440fc27d67c14e4e148a8126d5
MinusKelvin <mark.carlson@minuskelvin.net>
2023-03-16 19:28:50 +1100

    don't do lmp at pv nodes
    bench: 5144387

--- frozenight/src/search/negamax.rs
+++ frozenight/src/search/negamax.rs
@@ -93,6 +93,7 @@ impl Searcher<'_> {
         let mut best_mv = None;
         let mut raised_alpha = false;
         let mut quiets_remaining = match depth {
+            _ if search.pv() => 999,
             1 => LMP_DEPTH_1.get(),
             2 => LMP_DEPTH_2.get(),
             3 => LMP_DEPTH_3.get(),
